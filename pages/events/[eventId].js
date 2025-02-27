7dcc4c3de11ba1d78b963e733f5d10fe06ed06b2
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-02-27 13:37:30 +0100

    feat : add event item page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,32 @@
 import { useRouter } from "next/router";
+import EventContent from "../../components/event-detail/event-content";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
 
 function EventPage() {
   const router = useRouter();
   const { eventId } = router.query;
-  return <div>EventPage {eventId}</div>;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>Event not found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
 export default EventPage;
