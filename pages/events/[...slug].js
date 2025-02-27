06e1072486513b995dfa10b21f3eb554b6813234
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-02-27 18:22:34 +0100

    feat : add search event section and page to show search result

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,54 @@
 import { useRouter } from "next/router";
-
+import { useEffect } from "react";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../ui/button/button";
 function FilteredEventsPage() {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>FilteredEventsPage {slug}</div>;
+
+  if (!slug) {
+    return <p className="center">Loading...</p>;
+  }
+  if (
+    isNaN(slug[0]) ||
+    isNaN(slug[1]) ||
+    +slug[0] > 2030 ||
+    +slug[1] < 1 ||
+    +slug[1] > 12
+  ) {
+    return (
+      <>
+        <p>Invalid filter. Please adjust your values!</p>
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </>
+    );
+  }
+  const filteredEvents = getFilteredEvents({
+    year: +slug[0],
+    month: +slug[1],
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <p>No events found for the chosen filter!</p>
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </>
+    );
+  }
+  const date = new Date(+slug[0], +slug[1] - 1);
+  return (
+    <div>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </div>
+  );
 }
 
 export default FilteredEventsPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,17 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 function Events() {
   const featuredEvents = getAllEvents();
+  const router = useRouter();
+  const filteredEvents = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
 
   return (
     <div>
+      <EventSearch onSearch={filteredEvents} />
       <EventList items={featuredEvents} />
     </div>
   );
