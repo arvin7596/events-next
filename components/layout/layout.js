958c4c1025a83f8ac15170f3e237cafd1e8380a5
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-02-27 13:36:07 +0100

    feat : add layout and main header

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import MainHeader from "./main-header";
+
+function Layout(props) {
+  return (
+    <div>
+      <MainHeader />
+      <main>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
